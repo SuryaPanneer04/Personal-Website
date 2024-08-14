e9f334fd83b58a1b9187073acec7108a5e8cdede
Surya <suryapanneer04@gmail.com>
2024-08-14 14:11:15 +0530

    update the mediaQuery

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app =express();
 const port =7000;
 const {Pool,Client} =require('pg');
 const bodyParser = require('body-parser');
-const open =require('open');
 
 const client= new Client({
     user: "postgres",
@@ -12,8 +11,6 @@ const client= new Client({
     password: "surya",
     port: 5432,
 });
-
-
 
 app.use(express.static(__dirname));
 
@@ -30,12 +27,70 @@ app.post("/",(req,res)=>{
       const{txtName,txtEmail,txtPhone,txtMsg}=req.body
       client.connect()
       client.query('insert into contact values($1,$2,$3,$4)',[txtName,txtEmail,txtPhone,txtMsg],(err,res)=>{
-        console.log(err,res);
-        client.end()
-      })    
+        console.log(err,res);  
+      client.end()  
+      })       
       res.sendFile(__dirname+ "/index.html");
 })
 
 app.listen(port,() =>{
     console.log(`example app listening on port ${port}!`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const port = 7000;
+// const { Pool } = require('pg');
+// const bodyParser = require('body-parser');
+
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "portfolio",
+//     password: "surya",
+//     port: 5432,
+// });
+
+// app.use(express.static(__dirname));
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.get('/submit',function(req,res){
+//     console.log("data saved");
+// })
+// app.post("/", async (req, res) => {
+//     const { txtName, txtEmail, txtPhone, txtMsg } = req.body;    
+//     try {
+//         const client = await pool.connect();
+//         await client.query('INSERT INTO contact (name, email, phone, message) VALUES ($1, $2, $3, $4)', [txtName, txtEmail, txtPhone, txtMsg]);
+//         client.release(); // Release the client back to the pool
+//         console.log("Data saved");
+//     } catch (err) {
+//         console.error("Error saving data: ", err);
+//     }    
+//     res.sendFile(__dirname + "/index.html");
+// });
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}!`);
+// });
