d58cd66f430fb54a5d72591573022d267c6ba000
Surya <suryapanneer04@gmail.com>
2024-08-15 11:50:24 +0530

    change the size

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const client= new Client({
     password: "surya",
     port: 5432,
 });
-
 app.use(express.static(__dirname));
 
 app.get("/",(req, res)=>{
@@ -27,7 +26,7 @@ app.post("/",(req,res)=>{
       const{txtName,txtEmail,txtPhone,txtMsg}=req.body
       client.connect()
       client.query('insert into contact values($1,$2,$3,$4)',[txtName,txtEmail,txtPhone,txtMsg],(err,res)=>{
-        console.log(err,res);  
+      console.log(err,res);  
       client.end()  
       })       
       res.sendFile(__dirname+ "/index.html");
@@ -36,61 +35,3 @@ app.post("/",(req,res)=>{
 app.listen(port,() =>{
     console.log(`example app listening on port ${port}!`);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const app = express();
-// const port = 7000;
-// const { Pool } = require('pg');
-// const bodyParser = require('body-parser');
-
-// const pool = new Pool({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "portfolio",
-//     password: "surya",
-//     port: 5432,
-// });
-
-// app.use(express.static(__dirname));
-
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.get('/submit',function(req,res){
-//     console.log("data saved");
-// })
-// app.post("/", async (req, res) => {
-//     const { txtName, txtEmail, txtPhone, txtMsg } = req.body;    
-//     try {
-//         const client = await pool.connect();
-//         await client.query('INSERT INTO contact (name, email, phone, message) VALUES ($1, $2, $3, $4)', [txtName, txtEmail, txtPhone, txtMsg]);
-//         client.release(); // Release the client back to the pool
-//         console.log("Data saved");
-//     } catch (err) {
-//         console.error("Error saving data: ", err);
-//     }    
-//     res.sendFile(__dirname + "/index.html");
-// });
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}!`);
-// });
